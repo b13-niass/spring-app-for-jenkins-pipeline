ae7ef8b20e977d91407c0d48ea8ffe5eb26b2851
codec-13/ <barhamadieng66@gmail.com>
2025-01-28 17:17:03 +0000

    first commit

---
+++ src/main/java/com/codev/appforjenkinspipeline/AppForJenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.codev.appforjenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForJenkinsPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppForJenkinsPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codev/appforjenkinspipeline/AppForJenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codev.appforjenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppForJenkinsPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
